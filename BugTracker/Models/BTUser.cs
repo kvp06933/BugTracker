2e7d56ad1278bb1186868ccdd89afa219b161c65
Katherine Pitts <kvp06933@gmail.com>
2021-11-17 09:34:07 -0600

    Initial commit

---
+++ BugTracker/Models/BTUser.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTracker.Models
+{
+    public class BTUser : IdentityUser
+
+    {
+        [Required]
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+        [NotMapped]
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+
+            }
+        }
+        [NotMapped]
+        [DataType(DataType.Upload)]
+        public IFormFile Image { get; set; }
+        public string FileName { get; set; }
+        public byte ImageFile { get; set; }
+        public string ImageType { get; set; }
+    }
+}
